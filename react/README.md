6544c39f6fc02a87348f2650679aff659eb200e6
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-10-21 22:34:20 -0300

    Configuração Inicial

---
+++ react/README.md
@@ -0,0 +1,11 @@
+[Vite](https://vitejs.dev/guide/)
+
+##### Criando o projeto
+```
+yarn create vite react --template react-ts
+```
+
+##### Instação do SASS
+```
+yarn add sass
+```
---
+++ react/src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <main className={styles.contentWrapper}>
+      <h1>Hello World!</h1>
+    </main>
+  )
+}
+
+export { App }
