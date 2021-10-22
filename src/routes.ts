dfd0aa796c1f23841b17c5df165aa15bde5e1bc2
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-10-21 21:44:23 -0300

    Retorno das 3 últimas mensagens e Profile do User

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { AuthenticationUserController } from './controllers/AuthenticationUserController';
 import { CreateMessageController } from './controllers/CreateMessageControler';
+import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
 
 router.post('/authenticate', new AuthenticationUserController().handle);
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
+
+router.get('/messages/last3', new GetLast3MessagesController().handle);
+router.get('/profile/', ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
