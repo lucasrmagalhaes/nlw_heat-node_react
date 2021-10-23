333c06f6f5bf2414e440bd03a51c56719e9041d9
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-10-23 20:46:34 -0300

    Message List Layout

--- react/src/components/MessageList/index.tsx
+++ react/src/components/MessageList/index.tsx
@@ -1,8 +1,33 @@
 import styles from './styles.module.scss';
+import logoImg from '../../assets/logo.svg';
 
 function MessageList() {
     return (
-        <h1>MessageList</h1>
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/lucasrmagalhaes.png" alt="Lucas Magalhães" />
+                        </div>
+                        <span>Lucas Magalhães</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/lucasrmagalhaes.png" alt="Lucas Magalhães" />
+                        </div>
+                        <span>Lucas Magalhães</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
 
