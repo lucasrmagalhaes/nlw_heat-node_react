c495b2c2c62b99ec00f5ff76ac68be99e4b19985
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-10-25 23:32:09 -0300

    socket.io-client

--- react/src/components/MessageList/index.tsx
+++ react/src/components/MessageList/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
+import io from 'socket.io-client';
 import logoImg from '../../assets/logo.svg';
 
 import styles from './styles.module.scss';
@@ -13,8 +14,30 @@ type Message = {
     }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+    messagesQueue.push(newMessage);
+})
+
 function MessageList() {
     const [messages, setMessages] = useState<Message[]>([]);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (messagesQueue.length > 0) {
+                setMessages(prevState => [
+                    messagesQueue[0],
+                    prevState[0],
+                    prevState[1],
+                ].filter(Boolean))
+
+                messagesQueue.shift();
+            }
+        }, 3000)
+    }, [])
 
     useEffect(() => {
         api.get<Message[]>('messages/last3').then(response => {
